bc4c186c823aca9b9f983ae05fc05506aff33081
Administrador <Administrador@10.10.3.13>
2019-04-15 22:24:39 -0300

    Listagem Circuito Manutencao Circuito

--- Formula01/src/telas/manutencao/ManutencaoEquipe.java
+++ Formula01/src/telas/manutencao/ManutencaoEquipe.java
@@ -9,12 +9,12 @@ package telas.manutencao;
  *
  * @author Administrador
  */
-public class ManutençãoEquipe extends javax.swing.JDialog {
+public class ManutencaoEquipe extends javax.swing.JDialog {
 
     /**
      * Creates new form ManutençãoEquipe
      */
-    public ManutençãoEquipe(java.awt.Frame parent, boolean modal) {
+    public ManutencaoEquipe(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -149,20 +149,21 @@ public class ManutençãoEquipe extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ManutençãoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManutencaoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ManutençãoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManutencaoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ManutençãoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManutencaoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ManutençãoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ManutencaoEquipe.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                ManutençãoEquipe dialog = new ManutençãoEquipe(new javax.swing.JFrame(), true);
+                ManutencaoEquipe dialog = new ManutencaoEquipe(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
