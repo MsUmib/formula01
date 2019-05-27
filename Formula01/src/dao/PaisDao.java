6c97b331bdc394ec873babe7e6fb5d11fe51c956
Administrador <Administrador@10.10.3.13>
2019-05-27 15:58:00 -0300

    Alterado PaisDao

--- Formula01/src/dao/PaisDao.java
+++ Formula01/src/dao/PaisDao.java
@@ -11,6 +11,26 @@ import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class PaisDao {
+    public static List<String[]> consultar() 
+    {
+        List<String[]> resultados = new ArrayList<>();
+        String sql = "SELECT sigla, nome FROM pais";
+        PreparedStatement ps;
+        try {
+            ps = conexao.Conexao.getConexao().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+                String[] linha = new String[2];
+                linha[0] = rs.getString("sigla");
+                linha[1] = rs.getString("nome");
+                resultados.add(linha);
+            }
+            return resultados;
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(PaisDao.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
     public static boolean inserir(String sigla, String nome)
     {
         String sql = "INSERT INTO pais (sigla, nome) VALUES (?, ?)";
@@ -46,7 +66,7 @@ public class PaisDao {
             }
         }
     public static void main(String[] args) {
-        boolean resultado = alterar("BR", "brail");
+        boolean resultado = alterar("BR", "Brasil");
         if (resultado){
             JOptionPane.showMessageDialog(null, "Inserido com sucesso!");
         }else{
